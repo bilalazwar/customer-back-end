3f98afc3a997dc5be77e898019c17b2db7a42e46
Bilal Azwar <bilalazwar@gmail.com>
2024-01-28 08:19:30 +0530

    Initial commit

---
+++ src/test/java/com/TechTron/customerbackend/CustomerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechTron.customerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
